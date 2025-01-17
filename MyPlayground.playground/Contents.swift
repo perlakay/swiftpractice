719f974d5f7b680a4ff67528711f987f472cf5e7
perlakay <perla.kay22@gmail.com>
2025-01-17 10:58:28 -0500

    swift practice

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+var greeting:String = "Hello, playground"
+print(greeting)
+
+// really when you declare a variable, you are using the shorthand of name:type
+
+var myInt = 5
+print(myInt)
+
+var myDouble = 0.5
+print(myDouble)
+
+var myBool = false
+print(myBool)
+
+//the let keyword is used to declare a constant, the difference with a constant is that you cannot reassign it ever.
+
+let greeting2 = "I am a constant"
+print(greeting2)
---
+++ arrays.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+var a:Int = 5
+
+var b:[Int] = [5, 10, 15, 20, 25] //define an array with values
+//what data type is an array?
+//array is its own data type that stores a specific data type indicated by: varaiblename:[data type of data stored] = [values]
+
+print(a) //call a
+print(b[0]) //calling a specific index of the array, remember arrays start at 0
+
+print(b[4]) //calls last item
+
+//order is preserved in arrays
+
+//how do you assign new data?
+b[0] = 100 //assign new value to specific index
+
+print(b[0])
+
